e257a8063cddbb8e678b8ba51da7aa0c3f29a134
JISUOGIT <wltn203@naver.com>
2023-04-16 11:09:56 +0900

    20230416 - photo selected opacity 추가 - showModalBottomSheet 추가

--- lib/src/pages/upload.dart
+++ lib/src/pages/upload.dart
@@ -64,6 +64,7 @@ class _UploadState extends State<Upload> {
   Future<void> _pagingPhotos() async {
     var photos = await albums.first.getAssetListPaged(page: 0, size: 30);
     imageList.addAll(photos);
+    // 이미지 리스트의 첫번째
     selectedImage = imageList.first;
   }
 
@@ -74,21 +75,17 @@ class _UploadState extends State<Upload> {
       // Get.width / height
       width: width,
       height: width,
+      // null 일경우 container 리턴 아닐 경우 썸네일 로드
       child: selectedImage == null
           ? Container()
-          : FutureBuilder(
-              future: selectedImage!.thumbnailDataWithSize(
-                ThumbnailSize(width.toInt(), width.toInt()),
-              ),
-              builder: (context, AsyncSnapshot<Uint8List?> snapshot) {
-                if (snapshot.hasData) {
-                  return Image.memory(
-                    snapshot.data!,
-                    fit: BoxFit.cover,
-                  );
-                } else {
-                  return Container();
-                }
+          : _photoWidget(
+              selectedImage!,
+              width.toInt(),
+              builder: (data) {
+                return Image.memory(
+                  data!,
+                  fit: BoxFit.cover,
+                );
               },
             ),
     );
@@ -100,20 +97,71 @@ class _UploadState extends State<Upload> {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Padding(
-            padding: const EdgeInsets.all(5.0),
-            child: Row(
-              children: [
-                // 갤러리 선택시 photomanager로 직접 디바이스 내의 선택사항 가져올것
-                Text(
-                  headerTitle,
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 18,
+          GestureDetector(
+            onTap: () {
+              // bottomsheet modal로 띄우기
+              // 기본 color : white
+              showModalBottomSheet(
+                context: context,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20)),
+                ),
+                // isScrollControlled: true, bottomModal 사이즈를 전체로 띄우기
+                isScrollControlled: albums.length > 10 ? true : false,
+                // 전체 사이즈 - 상단 액션바
+                constraints: BoxConstraints(maxHeight: MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top),
+                builder: (context) => Container(
+                  // modal size 지정
+                  height: albums.length > 10 ? Size.infinite.height : albums.length * 60,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Center(
+                        child: Container(
+                          margin: EdgeInsets.only(top: 7),
+                          width: 40,
+                          height: 4,
+                          decoration: BoxDecoration(
+                              color: Colors.black54,
+                              borderRadius: BorderRadius.circular(10)),
+                        ),
+                      ),
+                      // 영역이 많아지면 overflow가 되기때문에 그에 해당하는 위젯에 SingleChildScrollView 넣기
+                      Expanded(
+                        child: SingleChildScrollView(
+                          child: Column(
+                            children: List.generate(
+                                albums.length,
+                                (index) => Container(
+                                      padding: EdgeInsets.symmetric(
+                                          vertical: 15, horizontal: 25),
+                                      child: Text(albums[index].name),
+                                    )).toList(),
+                          ),
+                        ),
+                      )
+                    ],
                   ),
                 ),
-                Icon(Icons.arrow_drop_down),
-              ],
+              );
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Row(
+                children: [
+                  // 갤러리 선택시 photomanager로 직접 디바이스 내의 선택사항 가져올것
+                  Text(
+                    headerTitle,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                    ),
+                  ),
+                  Icon(Icons.arrow_drop_down),
+                ],
+              ),
             ),
           ),
           Row(
@@ -161,35 +209,49 @@ class _UploadState extends State<Upload> {
     // SliverGridDelegateWithFixedCrossAxisCount :
     // singlechildscrollview & girdview 같이 사용할 경우 physics: NeverScrollableScrollPhysics(), 명시해서 방지하기
     return GridView.builder(
-        // 스크롤 방식 금지
-        physics: NeverScrollableScrollPhysics(),
-        shrinkWrap: true,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            // childAspectRatio : 숫자 비율에 따라 사각형 모양으로
-            crossAxisCount: 4,
-            // 간격조절
-            mainAxisSpacing: 1,
-            // 간격조절
-            crossAxisSpacing: 1,
-            // 간격조절
-            childAspectRatio: 1),
-        itemCount: imageList.length,
-        itemBuilder: (context, index) {
-          return _photoWidget(imageList[index]);
-        });
+      // 스크롤 방식 금지
+      physics: NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          // childAspectRatio : 숫자 비율에 따라 사각형 모양으로
+          crossAxisCount: 4,
+          // 간격조절
+          mainAxisSpacing: 1,
+          // 간격조절
+          crossAxisSpacing: 1,
+          // 간격조절
+          childAspectRatio: 1),
+      itemCount: imageList.length,
+      itemBuilder: (BuildContext context, int index) {
+        return _photoWidget(
+          imageList[index],
+          200,
+          builder: (data) => GestureDetector(
+            onTap: () {
+              selectedImage = imageList[index];
+              update();
+            },
+            child: Opacity(
+              // asset과 selectedImage가 같다면 opacity 주기
+              opacity: imageList[index] == selectedImage ? 0.3 : 1,
+              child: Image.memory(
+                data!,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        );
+      },
+    );
   }
 
-  Widget _photoWidget(AssetEntity asset) {
+  Widget _photoWidget(AssetEntity asset, int size,
+      {required Widget Function(Uint8List) builder}) {
     return FutureBuilder(
-      future: asset.thumbnailDataWithSize(
-        ThumbnailSize(200, 200),
-      ),
-      builder: (context, AsyncSnapshot<Uint8List?> snapshot) {
+      future: asset.thumbnailDataWithSize(ThumbnailSize(size, size)),
+      builder: (_, AsyncSnapshot<Uint8List?> snapshot) {
         if (snapshot.hasData) {
-          return Image.memory(
-            snapshot.data!,
-            fit: BoxFit.cover,
-          );
+          return builder(snapshot.data!);
         } else {
           return Container();
         }
